d4cc298ebc49ddb79ae93cb3af04ff4105aab25a
soul <hansol1501@gmail.com>
2025-04-30 08:39:57 +0900

    Initial Commit

---
+++ swiftuicombine/swiftuicombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuicombineApp.swift
+//  swiftuicombine
+//
+//  Created by air on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftuicombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
